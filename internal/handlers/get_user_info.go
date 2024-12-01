33a4d00b4a827a60c869d1667f951f467af1373e
timacias <147666644+timacias@users.noreply.github.com>
2024-11-30 22:46:26 -0500

    Fix `/app/userinfo`

--- internal/handlers/get_user_info.go
+++ internal/handlers/get_user_info.go
@@ -17,24 +17,32 @@ func getUserInfo(writer http.ResponseWriter, request *http.Request) {
 		api.InternalErrorHandler(writer)
 		return
 	}
-	usersCollection := tools.GetSessionCollection(store.DB)
+	usersCollection := tools.GetUserCollection(store.DB)
 
 	// Get an instance of the user's account
 	username := request.Context().Value("username").(string)
+
 	userAccount := tools.EmailInDatabase(username, usersCollection)
 	if err != nil || userAccount == nil {
 		api.InternalErrorHandler(writer)
 		return
 	}
 
+	// Debug hell
+	log.WithFields(log.Fields{
+		"username from request context": username,
+		"Returned account from DB":      userAccount,
+		"Error":                         err,
+	}).Info("HTTP request received")
+
 	var response = api.UserInfoResponse{
-		Code:		http.StatusOK,
-		Username:	userAccount.Email,
-		FirstName:	userAccount.FirstName,
-		LastName: 	userAccount.LastName,
-		InvitedBy:	userAccount.InvitedBy,
-		TakenDSA:	userAccount.TakenDSA,
-		Year:		userAccount.Year,
+		Code:        http.StatusOK,
+		Username:    userAccount.Email,
+		FirstName:   userAccount.FirstName,
+		LastName:    userAccount.LastName,
+		InvitedBy:   userAccount.InvitedBy,
+		TakenDSA:    userAccount.TakenDSA,
+		Year:        userAccount.Year,
 		Description: userAccount.Description,
 	}
 
